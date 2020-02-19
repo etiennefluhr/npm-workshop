1e813192c80a1a4f4cb157e65b2a4ab08e23b623
etiennefluhr <etienne.fluhr@arcor.de>
2020-02-19 11:06:39 +0100

    added physic engine

--- src/index.ts
+++ src/index.ts
@@ -1,1 +1,44 @@
-console.log( "src/index.ts being invoked" );
+import * as Matter from 'matter-js';
+
+/**
+ *   Demonstrates the usage of the MatterJS engine.
+ */
+class MatterJsDemo
+{
+    /**
+     *   Represents the entry point to our MatterJs demo.
+     *   This method creates a new MatterJs engine, adds two boxes along
+     *   with a static ground and starts the MatterJs physics simulation.
+     */
+    public static main()
+    {
+        let body :HTMLBodyElement = document.querySelector("body");
+        let engine :Matter.Engine = Matter.Engine.create( body );
+
+        let boxA :Matter.Body   = Matter.Bodies.rectangle( 400, 200, 80,  80 );
+        let boxB :Matter.Body   = Matter.Bodies.rectangle( 450, 50,  80,  80 );
+        let ground :Matter.Body = Matter.Bodies.rectangle( 400, 610, 810, 60, { isStatic: true } );
+
+        // add all bodies to the engine's world
+        Matter.World.add
+        (
+            engine.world,
+            [
+                boxA,
+                boxB,
+                ground,
+            ]
+        );
+
+        // start the MatterJS simulation
+        Matter.Engine.run( engine );
+    }
+}
+
+/**
+ *   Being invoked when all components of the website are loaded.
+ */
+window.onload = function()
+{
+    MatterJsDemo.main();
+};
